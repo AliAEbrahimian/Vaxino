9ebc7801742acc443861417846494138dbc2512a
ali aghaebrahimian <2aaghaebrahimian@gmail.com>
2020-10-18 00:31:06 +0330

    Initial Commit

--- app/src/main/java/com/aaebrahimian/vaxino/InformationClinicActivity.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/InformationClinicActivity.kt
@@ -1,17 +1,35 @@
 package com.aaebrahimian.vaxino
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class InformationClinicActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_information_clinic)
 
+        var img_Clinic = findViewById<ImageView>(R.id.img_Clinic)
+        var txt_Information = findViewById<TextView>(R.id.txt_Information_Clinic)
         val btn_Call = findViewById<Button>(R.id.btn_Call)
         val btn_Back = findViewById<Button>(R.id.btn_Back)
-        val btn_Information = findViewById<Button>(R.id.btn_Selection)
+        val btn_Selection = findViewById<Button>(R.id.btn_Selection)
+
+        btn_Call.setOnClickListener {
+
+        }
+
+        btn_Back.setOnClickListener {
+
+        }
+
+        btn_Selection.setOnClickListener {
+
+        }
+
 
     }
 }
--- app/src/main/java/com/aaebrahimian/vaxino/MainActivity.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/MainActivity.kt
@@ -4,14 +4,26 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        
+        val img_Main = findViewById<ImageView>(R.id.img_Main)
+        val txt_Main = findViewById<TextView>(R.id.txt_Main)
         val btn_Start_Vaccine = findViewById<Button>(R.id.btn_Start_Vaccine)
         val btn_Follow_Up = findViewById<Button>(R.id.btn_Follow_Up)
+
+        btn_Start_Vaccine.setOnClickListener {
+
+        }
+
+        btn_Follow_Up.setOnClickListener {
+
+        }
 
     }
 }
