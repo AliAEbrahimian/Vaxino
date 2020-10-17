2ec7d8b77b87c2913aaa4de289979fcf5d8c513a
ali aghaebrahimian <2aaghaebrahimian@gmail.com>
2020-10-17 20:46:22 +0330

    Initial Commit

--- app/src/main/java/com/aaebrahimian/vaxino/InformationClinicActivity.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/InformationClinicActivity.kt
@@ -3,9 +3,9 @@ package com.aaebrahimian.vaxino
 import android.app.Activity
 import android.os.Bundle
 
-class MainActivity : Activity() {
+class InformationClinicActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_information_clinic)
     }
 }
