427ec1b54119ae81db6daac852df9d4059a26339
ali aghaebrahimian <2aaghaebrahimian@gmail.com>
2020-10-24 01:03:42 +0330

    Initial Commit

--- app/src/main/java/com/aaebrahimian/vaxino/WelcomeSplash.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/WelcomeSplash.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import kotlin.concurrent.timerTask
 
 class WelcomeSplash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +14,11 @@ class WelcomeSplash : AppCompatActivity() {
 
         img_Welcome_Splash.alpha = 0f
         img_Welcome_Splash.animate().setDuration(4000).alpha(1f).withEndAction{
-
             val intent = Intent (this,MainActivity::class.java)
             startActivity(intent)
             overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
             finish()
-
-        }
+         }
 
     }
 }
