900a83332e0af29304796923a1dc7571dd247b91
ali aghaebrahimian <2aaghaebrahimian@gmail.com>
2020-10-24 19:56:02 +0330

    Initial Commit

--- app/src/main/java/com/aaebrahimian/vaxino/MainActivity.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/MainActivity.kt
@@ -15,11 +15,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val img_Main = findViewById<ImageView>(R.id.img_Main)
-
         val btn_Start_Vaccine = findViewById<Button>(R.id.btn_Start_Vaccine)
         val btn_Follow_Up = findViewById<Button>(R.id.btn_Follow_Up)
         val txt_Main = findViewById<TextView>(R.id.txt_Main)
 
+        txt_Main.alpha = 0f
+        txt_Main.animate().setDuration(1500).alpha(1f)
         txt_Main.typeface = ResourcesCompat.getFont(this,R.font.frission)
         txt_Main.text = "Description:\n" +
                 "By pressing the START VACCINE button you will enter the vaccination stage\n" +
--- app/src/main/java/com/aaebrahimian/vaxino/WelcomeSplash.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/WelcomeSplash.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import kotlin.concurrent.timerTask
 
 class WelcomeSplash : AppCompatActivity() {
@@ -11,9 +13,17 @@ class WelcomeSplash : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_splash)
         val img_Welcome_Splash = findViewById<ImageView>(R.id.img_Welcome_Splash)
+        val txt_Vaxino = findViewById<TextView>(R.id.txt_vaxino)
+
+        txt_Vaxino.text = "VAXINO"
+        txt_Vaxino.typeface = ResourcesCompat.getFont(this,R.font.pdark)
 
         img_Welcome_Splash.alpha = 0f
-        img_Welcome_Splash.animate().setDuration(4000).alpha(1f).withEndAction{
+        txt_Vaxino.alpha = 0f
+
+        img_Welcome_Splash.animate().setDuration(3000).alpha(1f)
+        txt_Vaxino.animate().setDuration(3000).alpha(1f).withEndAction{
+
             val intent = Intent (this,MainActivity::class.java)
             startActivity(intent)
             overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
