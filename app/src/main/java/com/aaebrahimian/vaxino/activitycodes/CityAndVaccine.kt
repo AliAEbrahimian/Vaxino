39f0bfffe20152cd5ddac98cae0c3de3a502a681
Parsa Mahmoodi <m.n.aparsam@gmail.com>
2020-11-27 01:20:48 +0330

    Added Statics.kt fixed Toast in Spinner onClickListener

--- app/src/main/java/com/aaebrahimian/vaxino/activitycodes/CityAndVaccine.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/activitycodes/CityAndVaccine.kt
@@ -6,20 +6,23 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import com.aaebrahimian.vaxino.R
+import com.aaebrahimian.vaxino.resources.Statics
+import kotlinx.android.synthetic.main.activity_cityandvaccine.*
+
 
 class CityAndVaccine : AppCompatActivity() {
+
+    val statics:Statics = Statics()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cityandvaccine)
 
 
-        val txt_City = findViewById<TextView>(R.id.txt_City)
         txt_City.alpha = 0f
         txt_City.animate().setDuration(1500).alpha(1f)
         txt_City.typeface = ResourcesCompat.getFont(this, R.font.frission)
         txt_City.text = "1. You can select your desired city by hitting the spinner below."
-
-        val spinner_City : Spinner = findViewById(R.id.spinner_City)
 
         ArrayAdapter.createFromResource(
             this,
@@ -37,7 +40,7 @@ class CityAndVaccine : AppCompatActivity() {
                 id: Long
             ) {
 
-                Toast.makeText( this@CityAndVaccine, "You select " , Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@CityAndVaccine, "You select ${statics.CityList[position]}", Toast.LENGTH_SHORT).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -47,13 +50,11 @@ class CityAndVaccine : AppCompatActivity() {
 
         }
 
-        val txt_Vaccine = findViewById<TextView>(R.id.txt_Vaccine)
         txt_Vaccine.alpha = 0f
         txt_Vaccine.animate().setDuration(1500).alpha(1f)
         txt_Vaccine.typeface = ResourcesCompat.getFont(this, R.font.frission)
         txt_Vaccine.text = "2. You can select the vaccine you want by hitting the spinner below."
 
-        val spinner_Vaccine : Spinner = findViewById(R.id.spinner_Vaccine)
         ArrayAdapter.createFromResource(
             this,
                 R.array.Vaccine_array,
@@ -63,7 +64,6 @@ class CityAndVaccine : AppCompatActivity() {
             spinner_Vaccine.adapter = adapter
         }
 
-        val btn_Selection = findViewById<Button>(R.id.btn_Selection)
         btn_Selection.setOnClickListener {
 
         }
---
+++ app/src/main/java/com/aaebrahimian/vaxino/resources/Statics.kt
@@ -0,0 +1,6 @@
+package com.aaebrahimian.vaxino.resources
+
+class Statics {
+    val CityList = listOf<String>("...", "Isfahan", "Shahin Shar", "Khomeini Shahr",
+                                  "Najaf Abad", "Khorasgun", "Sepahan Shahr", "Falavarjan")
+}
