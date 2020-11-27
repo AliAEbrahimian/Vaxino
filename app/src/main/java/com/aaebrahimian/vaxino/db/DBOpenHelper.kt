bf79716244ac4a4290b62a6d44163255d7946b72
ali aghaebrahimian <2aaghaebrahimian@gmail.com>
2020-11-28 00:06:18 +0330

    Write database

--- app/src/main/java/com/aaebrahimian/vaxino/db/DBOpenHelper.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/db/DBOpenHelper.kt
@@ -1,14 +1,17 @@
 package com.aaebrahimian.vaxino.db
 
+import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DBOpenHelper: SQLiteOpenHelper() {
+class DBOpenHelper(context: Context?) : SQLiteOpenHelper( context ,null ,null ,1 ) {
+
     override fun onCreate(db: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+        db?.execSQL("CREATE TABLE 'vaxino'(id INTEGER AUTOINCREMENT PRIMARY KEY NOT NULL , text_title TEXT , text_body TEXT , img_item )")
+
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
     }
+
 }
