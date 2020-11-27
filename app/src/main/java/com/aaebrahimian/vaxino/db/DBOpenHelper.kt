d07d610c17ded1fc3470dc58584f8befafa170da
Parsa Mahmoodi <m.n.aparsam@gmail.com>
2020-11-27 23:12:04 +0330

    Implemented members of SQLiteOpenHelper

--- app/src/main/java/com/aaebrahimian/vaxino/db/DBOpenHelper.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/db/DBOpenHelper.kt
@@ -1,6 +1,14 @@
 package com.aaebrahimian.vaxino.db
 
+import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DBOpenHelper: SQLiteOpenHelper() {
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
 }
