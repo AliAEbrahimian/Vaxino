56ee6104c5108b58dadf837afe8fecb8069f5c68
ali aghaebrahimian <2aaghaebrahimian@gmail.com>
2020-11-28 16:57:15 +0330

    Write database

--- app/src/main/java/com/aaebrahimian/vaxino/db/DBOpenHelper.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/db/DBOpenHelper.kt
@@ -6,44 +6,42 @@ import android.database.sqlite.SQLiteOpenHelper
 import android.widget.Toast
 import com.aaebrahimian.vaxino.model.Models
 
-class DBOpenHelper(context : Context?) :
-        SQLiteOpenHelper( context , null ,null ,1 ) {
 
+class DBOpenHelper(context : Context?) : SQLiteOpenHelper( context , null ,null , 1 ) {
 
     companion object{
-         const val DATABASE_VERSION = 1
-         const val DATABASE_NAME = "Vaxino.db"
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "Vaxino.db"
 
-         const val NAME_TABLE_CLINIC = "clinic"
-         const val COLUMN_NUMBER_OF_CLINIC = "id"
-         const val COLUMN_NAME_CLINIC = "text_title"
-         const val COLUMN_ADDRESS_CLINIC = "text_body"
-         const val COLUMN_IMAGE_CLINIC = "img_item"
+        const val NAME_TABLE_CLINIC = "clinic"
+        const val COLUMN_NUMBER_OF_CLINIC = "id"
+        const val COLUMN_NAME_CLINIC = "text_title"
+        const val COLUMN_ADDRESS_CLINIC = "text_body"
+        const val COLUMN_IMAGE_CLINIC = "img_item"
 
-         const val NAME_TABLE_PERSON = "person"
-         const val COLUMN_FIRST_NAME = "firstname"
-         const val COLUMN_LAST_NAME = "lastname"
-         const val COLUMN_BIRTH_DAY = "birthday"
-         const val COLUMN_ID = "id"
-         const val COLUMN_GENDER = "gender"
-         const val COLUMN_PHONE_NUMBER = "phone"
+        const val NAME_TABLE_PERSON = "person"
+        const val COLUMN_FIRST_NAME = "firstname"
+        const val COLUMN_LAST_NAME = "lastname"
+        const val COLUMN_BIRTH_DAY = "birthday"
+        const val COLUMN_ID = "id"
+        const val COLUMN_GENDER = "gender"
+        const val COLUMN_PHONE_NUMBER = "phone"
+
     }
 
 
 
     override fun onCreate(db: SQLiteDatabase?) {
-
-         val createClinicTable = "CREATE TABLE $NAME_TABLE_CLINIC($COLUMN_NUMBER_OF_CLINIC INTEGER  PRIMARY KEY AUTOINCREMENT NOT NULL, $COLUMN_NAME_CLINIC TEXT , $COLUMN_ADDRESS_CLINIC TEXT , $COLUMN_IMAGE_CLINIC IMAGE)"
+        val createClinicTable = "CREATE TABLE $NAME_TABLE_CLINIC($COLUMN_NUMBER_OF_CLINIC INTEGER  PRIMARY KEY AUTOINCREMENT NOT NULL, $COLUMN_NAME_CLINIC TEXT , $COLUMN_ADDRESS_CLINIC TEXT , $COLUMN_IMAGE_CLINIC IMAGE)"
         db?.execSQL(createClinicTable)
 
         val createPersonTable = "CREATE TABLE $NAME_TABLE_PERSON ($COLUMN_FIRST_NAME TEXT, $COLUMN_LAST_NAME TEXT,$COLUMN_BIRTH_DAY DATE,$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, $COLUMN_GENDER, BIT$COLUMN_PHONE_NUMBER NUMBER)"
         db?.execSQL(createPersonTable)
-
-
-
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db!!.execSQL("DROP TABLE IF EXISTS $NAME_TABLE_PERSON")
+        onCreate(db)
     }
 
     fun getDataSet (mCtx: Context) : ArrayList<Models>{
