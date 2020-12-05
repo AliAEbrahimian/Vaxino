70da221982d3b343cfe542b9f7a08edcda880712
ali aghaebrahimian <2aaghaebrahimian@gmail.com>
2020-12-06 00:41:22 +0330

    FIX LAYOUTS

--- app/src/main/java/com/aaebrahimian/vaxino/splashscreen/WelcomeSplash.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/splashscreen/WelcomeSplash.kt
@@ -2,28 +2,25 @@ package com.aaebrahimian.vaxino.splashscreen
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import com.aaebrahimian.vaxino.R
 import com.aaebrahimian.vaxino.activitycodes.MainActivity
+import kotlinx.android.synthetic.main.activity_welcome_splash.*
 
 class WelcomeSplash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_splash)
-        val img_Welcome_Splash = findViewById<ImageView>(R.id.img_Welcome_Splash)
-        val txt_Vaxino = findViewById<TextView>(R.id.txt_vaxino)
 
-        txt_Vaxino.text = "VAXINO"
-        txt_Vaxino.typeface = ResourcesCompat.getFont(this, R.font.pdark)
+        txt_vaxino.text = "VAXINO"
+        txt_vaxino.typeface = ResourcesCompat.getFont(this, R.font.pdark)
 
         img_Welcome_Splash.alpha = 0f
-        txt_Vaxino.alpha = 0f
+        txt_vaxino.alpha = 0f
 
         img_Welcome_Splash.animate().setDuration(2000).alpha(1f)
-        txt_Vaxino.animate().setDuration(2000).alpha(1f).withEndAction{
+        txt_vaxino.animate().setDuration(2000).alpha(1f).withEndAction{
 
             val intent = Intent (this, MainActivity::class.java)
             startActivity(intent)
