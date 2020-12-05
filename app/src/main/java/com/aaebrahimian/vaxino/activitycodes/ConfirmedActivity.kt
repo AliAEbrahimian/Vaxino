63eb6917222e1ed7c09143d9ba6b270d51376054
ali aghaebrahimian <2aaghaebrahimian@gmail.com>
2020-12-05 18:22:40 +0330

    Create Register Activity and Design Layout's

--- app/src/main/java/com/aaebrahimian/vaxino/activitycodes/ConfirmedActivity.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/activitycodes/ConfirmedActivity.kt
@@ -2,8 +2,6 @@ package com.aaebrahimian.vaxino.activitycodes
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.aaebrahimian.vaxino.R
 import kotlinx.android.synthetic.main.activity_confirmed.*
@@ -12,9 +10,6 @@ class ConfirmedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmed)
-
-        val txt_confirmed = findViewById<TextView>(R.id.txt_Confirmed)
-
 
         btn_Back.setOnClickListener {
             val intent = Intent (this, MainActivity::class.java)
--- app/src/main/java/com/aaebrahimian/vaxino/activitycodes/RegisterActivity.kt
+++ app/src/main/java/com/aaebrahimian/vaxino/activitycodes/RegisterActivity.kt
@@ -1,12 +1,16 @@
 package com.aaebrahimian.vaxino.activitycodes
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import com.aaebrahimian.vaxino.R
+import com.aaebrahimian.vaxino.db.DBOpenHelper
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -16,9 +20,13 @@ class RegisterActivity : AppCompatActivity() {
         txt_Register.typeface = ResourcesCompat.getFont(this, R.font.frission)
         txt_Register.text = "a"
 
+        var dbOpenHelper = DBOpenHelper(this,0,0)
+
         btn_Save.typeface = ResourcesCompat.getFont(this, R.font.frission)
         btn_Save.setOnClickListener {
 
+            var intent = Intent (this,ConfirmedActivity::class.java)
+            startActivity(intent)
         }
         btn_Cancel.typeface = ResourcesCompat.getFont(this, R.font.frission)
         btn_Cancel.setOnClickListener {
